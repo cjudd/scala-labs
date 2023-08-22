baefefdc32100d33d221e703b6c80af90db97410
Christopher M. Judd <javajudd@gmail.com>
2023-08-22 13:44:32 -0400

    Added Changed how we counted 1 to 10 lab.

--- src/main/scala/net/javajudd/scala/labs/datastructures/DataStructures.scala
+++ src/main/scala/net/javajudd/scala/labs/datastructures/DataStructures.scala
@@ -2,7 +2,7 @@ package net.javajudd.scala.labs.datastructures
 
 object DataStructures extends App {
 
-  val list = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val list = 1 to 10
 
   //val result = list.filter(_ % 2 == 0).reduce((x, y) => x + y)
   val result = list.filter(_ % 2 == 0).sum
